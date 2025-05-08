095319597cec808f067616947dd5b91d6f8bc911
Nicolas <nicolas.mollo.alanguia@cftmail.cl>
2025-05-08 13:35:20 -0400

    first commit

---
+++ Ejemplo_1/Models/DbCrudContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace Ejemplo_1.Models;
+
+public partial class DbCrudContext : DbContext
+{
+    public DbCrudContext()
+    {
+    }
+
+    public DbCrudContext(DbContextOptions<DbCrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+    //        => optionsBuilder.UseMySql("server=localhost;port=3306;database=db_crud;uid=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.32-mariadb"));
+    {
+        if (!optionsBuilder.IsConfigured) { }
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8_general_ci")
+            .HasCharSet("utf8");
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.IdUsuario).HasName("PRIMARY");
+
+            entity.ToTable("usuarios");
+
+            entity.Property(e => e.IdUsuario).HasColumnType("int(11)");
+            entity.Property(e => e.Apellido).HasMaxLength(45);
+            entity.Property(e => e.Correo).HasMaxLength(45);
+            entity.Property(e => e.Nombre).HasMaxLength(45);
+            entity.Property(e => e.Password).HasMaxLength(45);
+            entity.Property(e => e.Telefono).HasColumnType("int(11)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Ejemplo_1/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ejemplo_1.Models;
+
+public partial class Usuario
+{
+    public int IdUsuario { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public string Correo { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public DateOnly FechaRegistro { get; set; }
+
+    public int Telefono { get; set; }
+}
